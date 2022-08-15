bf6efe91220fc2e0f8e7f6de2d03a94358f2cdcc
Fady-Alwazir <fady.alwazir@gmail.com>
2022-08-15 17:17:10 +0300

    create common button and input Relates #6

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+import Button from './common/button';
+import Input from './common/input';
+
+export { Button, Input };
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { ThemeProvider } from '@mui/material';
 import App from './App';
+import { theme } from './themes';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>
 );
