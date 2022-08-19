0295ad2e2d47df0606cd77b816b9aa93ccb3b45c
farah2003 <farah.shaqoura@example.com>
2022-08-19 17:35:38 +0300

    header interfaces

--- src/interfaces/navbar.ts
+++ src/interfaces/navbar.ts
@@ -1,10 +1,24 @@
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction, ReactNode } from 'react';
 
 export interface Item {
   name: string;
   link: string;
 }
 export interface Props {
+  name: string;
+  items: Array<Item> | undefined;
+  Icon: ReactNode;
+  link: string | undefined;
+  selectedItem: string;
+  setSelectedItem: Dispatch<SetStateAction<string>>;
+}
+
+export interface ParentProps {
+  visible: boolean;
+  setVisible: Dispatch<SetStateAction<boolean>>;
+}
+
+export interface ChildProps {
   open: boolean;
   items: Array<Item>;
   selectedItem: string;
