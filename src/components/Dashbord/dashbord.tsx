a16104f59598dd23cf98843f37158dda8a7dd31f
farah2003 <farah.shaqoura@example.com>
2022-08-19 17:33:30 +0300

    Dashboard layout component

--- src/components/Dashbord/dashbord.tsx
+++ src/components/Dashbord/dashbord.tsx
@@ -1,19 +1,30 @@
-import { Box } from '@mui/material';
+import { useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { useMediaQuery } from '@material-ui/core';
 import SideNav from '../sideNav/sidenav';
 import Header from '../Header/header';
 import * as style from './style';
 
 const Dashboard = () => {
+  const [visible, setVisible] = useState(false);
+  const isDesktopScreen = useMediaQuery('(min-width: 1200px)');
+
+  useEffect(() => {
+    setVisible(isDesktopScreen);
+  }, [isDesktopScreen]);
+
   return (
     <Box sx={style.layout}>
-      <SideNav />
-      <Box style={style.main}>
-        <Header />
-        <Box style={style.content}>
-          <Outlet />
+      <>
+        <SideNav visible={visible} setVisible={setVisible} />
+        <Box sx={style.main}>
+          <Header setVisible={setVisible} />
+          <Box sx={style.content}>
+            <Outlet />
+          </Box>
         </Box>
-      </Box>
+      </>
     </Box>
   );
 };
--- src/components/Dashbord/style.ts
+++ src/components/Dashbord/style.ts
@@ -1,20 +1,34 @@
-export const main = {
-  marginLeft: '14vw',
+export const layout = {
+  display: 'flex',
+  flexDirection: 'column',
   height: '100vh',
-  width: '86vw',
+  width: '100vw',
+};
+export const main = {
+  marginLeft: {
+    xs: '0',
+    sm: '0',
+    md: '0',
+    lg: '20vw',
+    xl: '14vw',
+  },
+  height: '100vh',
+  width: {
+    xs: '100vw',
+    sm: '100vw',
+    md: '100vw',
+    lg: 'calc(100vw - 20vw)',
+    xl: 'calc(100vw - 14vw)',
+  },
+  backgroundColor: ['secondary.main'],
 };
 export const content = {
   width: '96%',
   height: '89%',
   margin: '.4% 2%',
-  borderRadius: '20px',
-  backgroundColor: '#ffff',
-};
-
-export const layout = {
-  display: 'flex',
-  flexDirection: 'column',
-  backgroundColor: '#F6F8FE',
-  height: '100vh',
-  width: '100vw',
+  borderRadius: {
+    lg: '20px',
+    xl: '20px',
+  },
+  backgroundColor: ['secondary.light'],
 };
