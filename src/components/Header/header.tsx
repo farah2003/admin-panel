b8db706ebccf4092972d25032273a782866f19f6
farah2003 <farah.shaqoura@example.com>
2022-08-19 17:34:09 +0300

    dashbord header component

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
 import { Avatar, Box } from '@mui/material';
+import { Menu } from '@mui/icons-material';
 import * as style from './style';
+import { HeaderInterface } from '../../interfaces';
 
-const Header = () => {
+const Header = ({ setVisible }: HeaderInterface.Props) => {
+  const handleDrawerVisablity = () => {
+    setVisible(true);
+  };
   return (
     <Box sx={style.header}>
+      <Menu sx={style.menuIcon} onClick={handleDrawerVisablity} />
       <Avatar sx={style.avater}>OP</Avatar>
     </Box>
   );
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -2,10 +2,28 @@ export const header = {
   height: '7%',
   padding: '1.5% 2.5%',
   display: 'flex',
+  flexDirection: 'row',
   alignItems: 'center',
-  justifyContent: 'flex-end',
+  justifyContent: {
+    xs: 'space-between',
+    sm: 'space-between',
+    md: 'space-between',
+    lg: 'flex-end',
+    xl: 'flex-end',
+  },
 };
 
 export const avater = {
-  bgcolor: 'red',
+  bgcolor: ['primary.main'],
+};
+
+export const menuIcon = {
+  display: {
+    xs: 'flex',
+    sm: 'flex',
+    md: 'flex',
+    lg: 'none',
+    xl: 'none',
+  },
+  cursor: 'pointer',
 };
