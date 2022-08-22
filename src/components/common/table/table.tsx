97b01537e084738ce68495076f796dcc96add46a
farah2003 <farah.shaqoura@example.com>
2022-08-22 03:02:07 +0300

    refactore table component

--- src/components/common/table/table.tsx
+++ src/components/common/table/table.tsx
@@ -1,95 +1,28 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState } from 'react';
 import {
   Box,
   Paper,
-  TableBody,
   Table,
   TableContainer,
-  TableRow,
-  TableCell,
-  Checkbox,
   TablePagination,
 } from '@mui/material';
 import TableToolBar from './tableToolBar';
 import TableHeader from './tableHeader';
-
-interface Data {
-  calories: number;
-  carbs: number;
-  fat: number;
-  name: string;
-  protein: number;
-}
-
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number
-): Data {
-  return {
-    name,
-    calories,
-    fat,
-    carbs,
-    protein,
-  };
-}
-const rows = [
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Donut', 452, 25.0, 51, 4.9),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Honeycomb', 408, 3.2, 87, 6.5),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Jelly Bean', 375, 0.0, 94, 0.0),
-  createData('KitKat', 518, 26.0, 65, 7.0),
-  createData('Lollipop', 392, 0.2, 98, 0.0),
-  createData('Marshmallow', 318, 0, 81, 2.0),
-  createData('Nougat', 360, 19.0, 9, 37.0),
-  createData('Oreo', 437, 18.0, 63, 4.0),
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Honeycomb', 408, 3.2, 87, 6.5),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Jelly Bean', 375, 0.0, 94, 0.0),
-  createData('KitKat', 518, 26.0, 65, 7.0),
-  createData('Lollipop', 392, 0.2, 98, 0.0),
-  createData('Marshmallow', 318, 0, 81, 2.0),
-  createData('Nougat', 360, 19.0, 9, 37.0),
-  createData('Oreo', 437, 18.0, 63, 4.0),
-];
+import TableBody from './tableBody';
+import * as style from './style';
+import { rows } from './fakeData';
 
 const GenericTable = () => {
   const [selected, setSelected] = useState<string[]>([]);
   const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(13);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
 
-  const handleChangePage = (event: unknown, newPage: number) => {
-    setPage(newPage);
-  };
-  const handleChangeRowsPerPage = (
-    event: React.ChangeEvent<HTMLInputElement>
+  const handleTableRowClick = (
+    event: React.MouseEvent<unknown>,
+    name: string
   ) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
-  const isSelected = (name: string) => selected.indexOf(name) !== -1;
-  const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.checked) {
-      const newSelected = rows.map((n) => n.name);
-      setSelected(newSelected);
-      return;
-    }
-    setSelected([]);
-  };
-
-  const handleClick = (event: React.MouseEvent<unknown>, name: string) => {
     const selectedIndex = selected.indexOf(name);
-    let newSelected: any = [];
+    let newSelected: string[] = [];
 
     if (selectedIndex === -1) {
       newSelected = newSelected.concat(selected, name);
@@ -106,74 +39,48 @@ const GenericTable = () => {
 
     setSelected(newSelected);
   };
+  const isSelected = (name: string) => {
+    return selected.includes(name);
+  };
+  const handleChangePage = (_: unknown, nextPage: number) => {
+    setPage(nextPage);
+  };
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.checked) {
+      const newSelected = rows.map((row) => row.name);
+      setSelected(newSelected);
+      return;
+    }
+    setSelected([]);
+  };
 
   return (
-    <Box
-      sx={{
-        width: '98%',
-        height: '98%',
-        mb: 2.5,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'flex',
-        backgroundColor: 'red',
-        borderRadius: '20px',
-      }}
-    >
-      <Paper sx={{ width: '100%', border: 'none' }} elevation={0}>
+    <Box sx={style.conatiner}>
+      <Paper sx={style.paper} elevation={0}>
         <TableToolBar numSelected={selected.length} />
-        <TableContainer>
+        <TableContainer sx={style.tableContainer}>
           <Table>
             <TableHeader
               onSelectAllClick={handleSelectAllClick}
               numSelected={selected.length}
               rowCount={rows.length}
             />
-            <TableBody>
-              {rows
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row, index) => {
-                  const isItemSelected = isSelected(row.name);
-                  const labelId = `enhanced-table-checkbox-${index}`;
-
-                  return (
-                    <TableRow
-                      hover
-                      onClick={(event) => handleClick(event, row.name)}
-                      role="checkbox"
-                      aria-checked={isItemSelected}
-                      tabIndex={-1}
-                      key={row.name}
-                      selected={isItemSelected}
-                    >
-                      <TableCell padding="checkbox">
-                        <Checkbox
-                          color="primary"
-                          checked={isItemSelected}
-                          inputProps={{
-                            'aria-labelledby': labelId,
-                          }}
-                        />
-                      </TableCell>
-                      <TableCell scope="row" padding="none">
-                        {row.name}
-                      </TableCell>
-                      <TableCell align="right">{row.calories}</TableCell>
-                      <TableCell align="right">{row.fat}</TableCell>
-                      <TableCell align="right">{row.carbs}</TableCell>
-                      <TableCell align="right">{row.protein}</TableCell>
-                    </TableRow>
-                  );
-                })}
-            </TableBody>
+            <TableBody
+              rows={rows}
+              handleTableRowClick={handleTableRowClick}
+              isSelected={isSelected}
+            />
           </Table>
         </TableContainer>
         <TablePagination
-          sx={{
-            borderBottom: 'none',
-            alignContent: 'center',
-            display: 'flex',
-          }}
+          sx={style.tablePagination}
           rowsPerPageOptions={[10, 20, 50]}
           count={rows.length}
           rowsPerPage={rowsPerPage}
