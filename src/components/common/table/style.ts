5fdb96af1eb5b9dd5b36bf60bff3e3446c58b563
farah2003 <farah.shaqoura@example.com>
2022-08-22 03:00:24 +0300

    add genral style for table

---
+++ src/components/common/table/style.ts
@@ -0,0 +1,58 @@
+export const conatiner = {
+  width: '100%',
+  height: '98%',
+  mb: 2.5,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'flex',
+  borderRadius: {
+    lg: '20px',
+    xl: '20px',
+  },
+};
+export const paper = {
+  width: '100%',
+  border: 'none',
+  borderRadius: {
+    lg: '20px',
+    xl: '20px',
+  },
+};
+
+export const tablePagination = {
+  borderBottom: 'none',
+};
+
+export const tableContainer = {
+  height: {
+    xs: '87%',
+    sm: '87%',
+    md: '87%',
+    lg: '85%',
+    xl: '85%',
+  },
+  '&::-webkit-scrollbar': {
+    width: 5,
+    height: 5,
+  },
+  '&::-webkit-scrollbar-track': {
+    backgroundColor: '#c2afe9',
+    borderRadius: 2,
+  },
+  '&::-webkit-scrollbar-thumb': {
+    backgroundColor: '#c2afe9',
+    borderRadius: 2,
+  },
+};
+
+export const Toolbar = {
+  pl: { sm: 2 },
+  pr: { xs: 1, sm: 1 },
+  borderRadius: {
+    lg: '20px 20px 0 0',
+    xl: '20px 20px 0 0',
+  },
+};
+export const Typography = {
+  flex: '1 1 100%',
+};
